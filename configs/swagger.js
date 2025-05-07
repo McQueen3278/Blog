59ef005595e0d59717825f5e5f98ef03801f668e
hrodriguez-2023278 <hrodriguez-2023278@kinal.edu.gt>
2025-05-06 23:29:33 -0600

    build: basic configuration from server

---
+++ configs/swagger.js
@@ -0,0 +1,29 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+
+const options ={
+    swaggerDefinition:{
+        openapi:"3.0.0",
+        info:{
+            title: "Blog ",
+            version: "1.0.0",
+            description: "API para blog personal",
+            contact:{
+                name: "Harol Rodriguez",
+                email: "hrodriguez-2023278@kinal.edu.gt"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3000/blog-personal/v1"
+            }
+        ]
+    },
+    apis:[
+        
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(options)
+
+export { swaggerDocs, swaggerUI}
