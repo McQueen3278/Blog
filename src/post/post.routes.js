7b080fac0c9717e84fdd61cacff10619b72a75ac
hrodriguez-2023278 <hrodriguez-2023278@kinal.edu.gt>
2025-05-09 01:33:22 -0600

    feat: route get post by comments is added

--- src/post/post.routes.js
+++ src/post/post.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createPost } from "./post.controller.js";
+import { createPost, getPostWithComments } from "./post.controller.js";
 import { postValidator } from "../middlewares/post-validator.js";
 import { uploadCourseDocument } from "../middlewares/multer-upload.js";
 
 const router = Router();
 
 router.post("/addPost", uploadCourseDocument.single("document"), postValidator, createPost);
+
+router.get("/getPostWithComments/:id", getPostWithComments);
 
 export default router;
